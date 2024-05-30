a6edfb6196d6875039d459721e0042b3e20d8adf
codeavl <codeavlmail@gmail.com>
2024-05-30 16:35:57 +0530

    basic stylings (padding, margin, borders ..) added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import * as $ from './styles/styles';
 
 function App() {
 
   return (
-    <>
+    <div>
       <h1>Hello world</h1>
-    </>
+      <div style={{...$.w250px, ...$.h200px, borderStyle: 'solid', ...$.b2, ...$.p6, ...$.br12}}>
+        <div style={{...$.w25, ...$.h25, borderStyle: 'solid', ...$.b3, ...$.bbr10}}>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
